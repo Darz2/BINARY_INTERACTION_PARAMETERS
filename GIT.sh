36bee06c15495cc87f0a2d3863f9365c9f6ca676
Darz2 <d.raju@tudelft.nl>
2026-02-26 15:09:04 +0100

    update-FEB-2026_BIP

---
+++ GIT.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git init
+git add *
+git commit -m "update-FEB-2026_BIP"
+git remote add origin https://github.com/Darz2/BINARY_INTERCTION_PARAMETERS.git
+git push -u origin main
---
+++ KIJ/README.txt
@@ -0,0 +1,2 @@
+BASE CASE = CO2_Ar.ipynb
+KIJ_TEST_FILE = KIJ_matrix.ipynb

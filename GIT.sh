f567ffa404cef50c2256f9b6fd407a771f970abe
D.Raju <d.raju@tudelft.nl>
2026-03-15 02:45:42 +0100

    update from backup

--- GIT.sh
+++ GIT.sh
@@ -4,4 +4,4 @@ git init
 git add *
 git commit -m "update from backup"
 # git remote add origin https://github.com/Darz2/BINARY_INTERACTION_PARAMETERS.git
-git push -u origin main
+git push -u origin master
